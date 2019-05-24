70ce320de969e1134093b8bd7a33daf8185a3ff4
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-05-25 01:24:46 +0300

    Add files via upload

---
+++ lesson03/task03/Converter.java
@@ -0,0 +1,13 @@
+public class Converter{
+	public static double convertKilogramToMilligram(double x){
+		return x * 1000000;
+	}
+
+	public static double convertKilogramToGram(double x){
+		return x * 1000;
+	}
+
+	public static double convertKilogramToTon(double x){
+		return x * 0.001;
+	}
+}
---
+++ lesson03/task03/Task3.java
@@ -0,0 +1,8 @@
+public class Task3{
+	public static void main(String[] args) {
+		double kilogram = 34;
+		Printer.print(kilogram + " Kilogram = " + Converter.convertKilogramToMilligram(kilogram) + " Milligram");
+		Printer.print(kilogram + " Kilogram = " + Converter.convertKilogramToGram(kilogram) + " Gram");
+		Printer.print(kilogram + " Kilogram = " + Converter.convertKilogramToTon(kilogram) + " Ton");
+	}
+}
