8cac910294c462f3c3d82d8deaf22c83ddc4898d
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-05-25 01:26:17 +0300

    Update Task3.java

--- lesson03/task03/Task3.java
+++ lesson03/task03/Task3.java
@@ -4,5 +4,5 @@ public class Task3{
 		Printer.print(kilogram + " Kilogram = " + Converter.convertKilogramToMilligram(kilogram) + " Milligram");
 		Printer.print(kilogram + " Kilogram = " + Converter.convertKilogramToGram(kilogram) + " Gram");
 		Printer.print(kilogram + " Kilogram = " + Converter.convertKilogramToTon(kilogram) + " Ton");
-	}
+	} 
 }
