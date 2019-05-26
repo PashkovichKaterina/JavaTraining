adc38049351325a48f74ed0998fa41c6d9c646a2
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-05-26 09:36:54 +0300

    Add files via upload

---
+++ lesson04/task04/Task4.java
@@ -0,0 +1,7 @@
+public class Task4{
+	public static void main(String[] args){
+		int number = 556877;
+		Printer.print("Arithmetic average of number " + number + " is " + NumberLogic.arithmeticAverage(number));
+		Printer.print("Geometrical average of number " + number + " is " + NumberLogic.geometricalAverage(number));
+	}
+}
