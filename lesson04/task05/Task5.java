ac94b9a364086dca64f56f3d791ce9e65b734a7b
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-05-26 09:38:38 +0300

    Add files via upload

---
+++ lesson04/task05/Task5.java
@@ -0,0 +1,6 @@
+public class Task5{
+	public static void main(String[] args){
+		int number = 1234567;
+		Printer.print("The number " + number + " reverse to " + NumberLogic.reverseNumber(number));
+	}
+}
