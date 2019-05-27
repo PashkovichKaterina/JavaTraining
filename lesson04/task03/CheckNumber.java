e5c9f8c253f0d890e7a9b34ce538a2c962ff9bdb
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-05-27 23:42:41 +0300

    Update CheckNumber.java

--- lesson04/task03/CheckNumber.java
+++ lesson04/task03/CheckNumber.java
@@ -1,25 +1,35 @@
 public class CheckNumber{
-	public static int calcDigit(int number, int position){
-		return (int)(number / Math.pow(10, position - 1) % 10);
-	}
-
 	public static boolean isDecreasing(int number)
 	{
-		int digit1 = calcDigit(number, 1);
-		int digit2 = calcDigit(number, 2);
-		int digit3 = calcDigit(number, 3);
-		int digit4 = calcDigit(number, 4);
+		int digit1 = number % 10;
+		number /= 10;
 
-		return (digit4 > digit3) && (digit3 > digit2) && (digit2 > digit1);
+		int digit2 = number % 10;
+		number /= 10;
+
+		int digit3 = number % 10;
+		number /= 10;
+
+		int digit4 = number % 10;
+		number /= 10;
+
+		return digit4 > digit3 && digit3 > digit2 && digit2 > digit1;
 	}
 
 	public static boolean isIncreasing(int number)
 	{
-		int digit1 = calcDigit(number, 1);
-		int digit2 = calcDigit(number, 2);
-		int digit3 = calcDigit(number, 3);
-		int digit4 = calcDigit(number, 4);
+		int digit1 = number % 10;
+		number /= 10;
 
-		return (digit4 < digit3) && (digit3 < digit2) && (digit2 < digit1);
+		int digit2 = number % 10;
+		number /= 10;
+
+		int digit3 = number % 10;
+		number /= 10;
+
+		int digit4 = number % 10;
+		number /= 10;
+
+		return digit4 < digit3 && digit3 < digit2 && digit2 < digit1;
 	}
 }
