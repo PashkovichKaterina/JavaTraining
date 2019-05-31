2b4d8b370e7be0821d4b3ba619df3fbbfa4b4bcc
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-05-31 21:31:54 +0300

    Update MoodSensor.java

--- lesson05/task3/src/by/epam/javatraining/pashkovich/lesson05/task03/model/MoodSensor.java
+++ lesson05/task3/src/by/epam/javatraining/pashkovich/lesson05/task03/model/MoodSensor.java
@@ -1,10 +1,10 @@
 package by.epam.javatraining.pashkovich.lesson05.task03.model;
 
 public class MoodSensor {
-    private static double BAD = 0.2;
-    private static double SO_SO = 0.4;
-    private static double GOOD = 0.6;
-    private static double EXCELLENT = 0.9;
+    final private static double BAD = 0.2;
+    final private static double SO_SO = 0.4;
+    final private static double GOOD = 0.6;
+    final private static double EXCELLENT = 0.9;
 
     public static String moodRandom() {
         double moodNumber = Randomizer.randomNumber();
