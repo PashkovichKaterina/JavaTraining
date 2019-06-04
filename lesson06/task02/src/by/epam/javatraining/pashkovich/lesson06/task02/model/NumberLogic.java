2d15939c0b30206acd17fb13433fc3f3fa7ccaf6
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-04 10:05:19 +0300

    Update NumberLogic.java

--- lesson06/task02/src/by/epam/javatraining/pashkovich/lesson06/task02/model/NumberLogic.java
+++ lesson06/task02/src/by/epam/javatraining/pashkovich/lesson06/task02/model/NumberLogic.java
@@ -3,28 +3,24 @@ package by.epam.javatraining.pashkovich.lesson06.task02.model;
 public class NumberLogic {
     public static boolean isDecreasing(int number) {
         number = Math.abs(number);
-        int digit = number % 10;
-        number /= 10;
-        while (number > 0) {
+        while (number / 10 > 0) {
+            int digit = number % 10;
+            number /= 10;
             if (digit >= number % 10) {
                 return false;
             }
-            digit = number % 10;
-            number /= 10;
         }
         return true;
     }
 
     public static boolean isIncreasing(int number) {
         number = Math.abs(number);
-        int digit = number % 10;
-        number /= 10;
-        while (number > 0) {
+        while (number / 10 > 0) {
+            int digit = number % 10;
+            number /= 10;
             if (digit <= number % 10) {
                 return false;
             }
-            digit = number % 10;
-            number /= 10;
         }
         return true;
     }
