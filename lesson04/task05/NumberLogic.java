9370a2fc7b3363cb9ad489a4f16264fb8112bad3
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-06 15:50:07 +0300

    Update NumberLogic.java

--- lesson04/task05/NumberLogic.java
+++ lesson04/task05/NumberLogic.java
@@ -1,26 +1,26 @@
 public class NumberLogic{	
 	public static int reverseNumber(int number){
-		int digit1 = number % 10 * 1_000_000;
+		int newNumber = number % 10 * 1_000_000;
 		number /= 10;
 
-		int digit2 = number % 10 * 100_000;
+		newNumber += number % 10 * 100_000;
 		number /= 10;
 
-		int digit3 = number % 10 * 10_000;
+		newNumber += number % 10 * 10_000;
 		number /= 10;
 
-		int digit4 = number % 10 * 1_000;
+		newNumber += number % 10 * 1_000;
 		number /= 10;
 
-		int digit5 = number % 10 * 100;
+		newNumber += number % 10 * 100;
 		number /= 10;
 
-		int digit6 = number % 10 *10;
+		newNumber += number % 10 *10;
 		number /= 10;
 
-		int digit7 = number % 10;
+		newNumber += number % 10;
 		number /= 10;
 
-		return digit1 + digit2 + digit3 + digit4 + digit5 + digit6 + digit7;
+		return newNumber;
 	}
 }
