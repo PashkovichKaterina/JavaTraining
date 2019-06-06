dc40e75c5dba42338689ec2e8b4196384661078c
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-06 15:44:30 +0300

    Update NumberLogic.java

--- lesson04/task04/NumberLogic.java
+++ lesson04/task04/NumberLogic.java
@@ -4,46 +4,46 @@ public class NumberLogic{
 	private static final int COUNT = 6;
 
 	public static double arithmeticAverage(int number){
-		int digit1 = number % 10;
+		int sum = number % 10;
 		number /= 10;
 
-		int digit2 = number % 10;
+		sum += number % 10;
 		number /= 10;
 
-		int digit3 = number % 10;
+		sum += number % 10;
 		number /= 10;
 
-		int digit4 = number % 10;
+		sum += number % 10;
 		number /= 10;
 
-		int digit5 = number % 10;
+		sum += number % 10;
 		number /= 10;
 
-		int digit6 = number % 10;
+		sum += number % 10;
 		number /= 10;
 
-		return (digit1 + digit2 + digit3 + digit4 + digit5 + digit6) / (double)COUNT;
+		return sum / (double)COUNT;
 	}
 	
 	public static double geometricalAverage(int number){
-		int digit1 = number % 10;
+		int multiply = number % 10;
 		number /= 10;
 
-		int digit2 = number % 10;
+		multiply *= number % 10;
 		number /= 10;
 
-		int digit3 = number % 10;
+		multiply *= number % 10;
 		number /= 10;
 
-		int digit4 = number % 10;
+		multiply *= number % 10;
 		number /= 10;
 
-		int digit5 = number % 10;
+		multiply *= number % 10;
 		number /= 10;
 
-		int digit6 = number % 10;
+		multiply *= number % 10;
 		number /= 10;
 
-		return Math.pow(digit1 * digit2 * digit3 * digit4 * digit5 * digit6, 1 / (double)COUNT);
+		return Math.pow(multiply, 1 / (double)COUNT);
 	}
 }
