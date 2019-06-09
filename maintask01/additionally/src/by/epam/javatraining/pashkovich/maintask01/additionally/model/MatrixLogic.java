82580f7c653ddbe8fe9ed560822c5ace742c5b90
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-09 23:36:16 +0300

    Add files via upload

---
+++ maintask01/additionally/src/by/epam/javatraining/pashkovich/maintask01/additionally/model/MatrixLogic.java
@@ -0,0 +1,103 @@
+package by.epam.javatraining.pashkovich.maintask01.additionally.model;
+
+/**
+ * This class contains different method for work with matrix.
+ *
+ * @author Pashkovich Katsiaryna Vasilevna
+ * @version 1.0.0
+ */
+public class MatrixLogic {
+    /**
+     * This method finds the maximum value in the matrix.
+     *
+     * @param matrix The matrix in which the maximum value will be found.
+     * @return The maximum value in the matrix, or Double.MIN_VALUE if the matrix length is 0
+     */
+    public static double findMaxValue(double[][] matrix) {
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    /**
+     * This method finds the minimum value in the matrix.
+     *
+     * @param matrix The matrix in which the minimum value will be found.
+     * @return The minimum value in the matrix, or Double.MAX_VALUE if the matrix length is 0
+     */
+    public static double findMinValue(double[][] matrix) {
+        double min = Double.MAX_VALUE;
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+        }
+        return min;
+    }
+
+    /**
+     * This method checks is the matrix square.
+     *
+     * @param matrix The matrix which will be checked.
+     * @return True if the matrix is square, or false if the matrix is not square ot the matrix length is 0
+     */
+    public static boolean isSquare(double[][] matrix) {
+        if (matrix.length == 0){
+            return false;
+        }
+        for (int i = 0; i < matrix.length; ++i) {
+            if (matrix.length != matrix[i].length) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * This method checks is the matrix symmetric relative to the main diagonal.
+     *
+     * @param matrix The matrix which will be checked
+     * @return True if the matrix symmetric relative to the main diagonal,
+     * or false if the matrix not symmetric relative to the main diagonal
+     */
+    public static boolean isSymmetric(double[][] matrix) {
+        if (!isSquare(matrix)) {
+            return false;
+        }
+        for (int i = 0; i < matrix.length - 1; ++i) {
+            for (int j = i + 1; j < matrix[i].length; ++j) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * This method transposes matrix.
+     *
+     * @param matrix The matrix which will be transposed.
+     */
+    public static void transpose(double[][] matrix) {
+        if (!isSquare(matrix)) {
+            return;
+        }
+        for (int i = 0; i < matrix.length - 1; ++i) {
+            for (int j = i + 1; j < matrix[i].length; ++j) {
+                matrix[i][j] += matrix[j][i];
+                matrix[j][i] = matrix[i][j] - matrix[j][i];
+                matrix[i][j] -= matrix[j][i];
+            }
+        }
+    }
+}
+
---
+++ maintask01/additionally/test/by/epam/javatraining/pashkovich/maintask01/additionally/model/MatrixLogicTest.java
@@ -0,0 +1,176 @@
+package by.epam.javatraining.pashkovich.maintask01.additionally.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * This class contains unit-tests for checks class MatrixLogic
+ *
+ * @author Pashkovich Katsiaryna Vasilevna
+ * @version 1.0.0
+ */
+public class MatrixLogicTest {
+
+    @Test
+    public void testFindMaxValue() {
+        double[][] matrix = {
+                {5, 97, 4, 6, 4},
+                {4, 6, 8, 7},
+                {6, 5, 7, 2}
+        };
+        double expected = 97;
+        assertEquals(expected, MatrixLogic.findMaxValue(matrix), 0.1);
+    }
+
+    @Test
+    public void testFindMaxValueZero() {
+        double[][] matrix = {};
+        double expected = Double.MIN_VALUE;
+        assertEquals(expected, MatrixLogic.findMaxValue(matrix), 0.1);
+    }
+
+    @Test
+    public void testFindMinValue() {
+        double[][] matrix = {
+                {5, 97, 4, 6, 4},
+                {4, 6, 2, 7},
+                {6, 5, 7, 2}
+        };
+        double expected = 2;
+        assertEquals(expected, MatrixLogic.findMinValue(matrix), 0.1);
+    }
+
+    @Test
+    public void testFindMinValueZero() {
+        double[][] matrix = {};
+        double expected = Double.MAX_VALUE;
+        assertEquals(expected, MatrixLogic.findMinValue(matrix), 0.1);
+    }
+
+    @Test
+    public void testIsSquare() {
+        double[][] matrix = {
+                {5, 7, 8, 9, 5},
+                {6, 7, 8, 2, 1},
+                {4, 3, 6, 9, 2},
+                {5, 7, 3, 9, 6},
+                {8, 7, 2, 6, 4}
+        };
+        boolean expected = true;
+        assertEquals(expected, MatrixLogic.isSquare(matrix));
+    }
+
+    @Test
+    public void testIsSquareNot1() {
+        double[][] matrix = {
+                {5, 7, 8, 9, 5},
+                {6, 7, 8, 2, 1},
+                {4, 3, 9, 2},
+                {5, 7, 3, 9, 6},
+                {8, 7, 2, 6}
+        };
+        boolean expected = false;
+        assertEquals(expected, MatrixLogic.isSquare(matrix));
+    }
+
+    @Test
+    public void testIsSquareNot2() {
+        double[][] matrix = {
+                {5, 7, 8, 9, 5},
+                {6, 7, 8, 2, 1},
+                {5, 7, 3, 9, 6}
+        };
+        boolean expected = false;
+        assertEquals(expected, MatrixLogic.isSquare(matrix));
+    }
+
+    @Test
+    public void testIsSquareZero() {
+        double[][] matrix = {};
+        boolean expected = false;
+        assertEquals(expected, MatrixLogic.isSquare(matrix));
+    }
+
+    @Test
+    public void testIsSymmetric() {
+        double[][] matrix = {
+                {5, 7, 8, 9},
+                {7, 7, 8, 2},
+                {8, 8, 3, 9},
+                {9, 2, 9, 9}
+        };
+        boolean expected = true;
+        assertEquals(expected, MatrixLogic.isSymmetric(matrix));
+    }
+
+    @Test
+    public void testIsSymmetricNot() {
+        double[][] matrix = {
+                {5, 7, 8, 9},
+                {7, 7, 8, 3},
+                {5, 8, 3, 9},
+                {9, 2, 9, 9}
+        };
+        boolean expected = false;
+        assertEquals(expected, MatrixLogic.isSymmetric(matrix));
+    }
+
+    @Test
+    public void testIsSymmetricNotSquare() {
+        double[][] matrix = {
+                {5, 7, 8, 9},
+                {7, 7, 8, 2},
+                {8, 8, 3, 9}
+        };
+        boolean expected = false;
+        assertEquals(expected, MatrixLogic.isSymmetric(matrix));
+    }
+
+    @Test
+    public void testIsSymmetricZero() {
+        double[][] matrix = {};
+        boolean expected = false;
+        assertEquals(expected, MatrixLogic.isSymmetric(matrix));
+    }
+
+    @Test
+    public void testTranspose() {
+        double[][] matrix = {
+                {5, 7, 8, 9},
+                {7, 7, 8, 3},
+                {5, 8, 3, 9},
+                {9, 2, 9, 9}
+        };
+        MatrixLogic.transpose(matrix);
+        double[][] expected = {
+                {5, 7, 5, 9},
+                {7, 7, 8, 2},
+                {8, 8, 3, 9},
+                {9, 3, 9, 9}
+        };
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                assertEquals(expected[i][j], matrix[i][j], 0.1);
+            }
+        }
+    }
+
+    @Test
+    public void testTransposeNotSquare() {
+        double[][] matrix = {
+                {5, 7, 8, 9},
+                {7, 7, 8, 3}
+        };
+        MatrixLogic.transpose(matrix);
+        double[][] expected = {
+                {5, 7, 8, 9},
+                {7, 7, 8, 3}
+        };
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[i].length; ++j) {
+                assertEquals(expected[i][j], matrix[i][j], 0.1);
+            }
+        }
+    }
+}
