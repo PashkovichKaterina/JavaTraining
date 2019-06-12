73a8b6bd8295bf05fae4c0f1677139b5560cc649
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-13 01:42:49 +0300

    Update HeadOrTail.java

--- lesson06/task01/src/by/epam/javatraining/lesson06/task01/model/HeadOrTail.java
+++ lesson06/task01/src/by/epam/javatraining/lesson06/task01/model/HeadOrTail.java
@@ -3,21 +3,16 @@ package by.epam.javatraining.lesson06.task01.model;
 import java.util.Random;
 
 public class HeadOrTail {
-    private static final int HEAD = 0;
-    private static final int SIDES = 2;
+    private static final boolean HEAD = true;
 
-    public static String headsORTails(int steps) {
+    public static int calcHead(int steps) {
         Random random = new Random();
-        String result = "Invalid data";
-        if (steps >= 0) {
-            int head = 0;
-            for (int i = 0; i < steps; ++i) {
-                if (random.nextInt(SIDES + 1) == HEAD) {
-                    ++head;
-                }
+        int head = 0;
+        for (int i = 0; i < steps; ++i) {
+            if (random.nextBoolean() == HEAD) {
+                ++head;
             }
-            result = "Heads: " + head + " Tails: " + (steps - head);
         }
-        return result;
+        return head;
     }
 }
