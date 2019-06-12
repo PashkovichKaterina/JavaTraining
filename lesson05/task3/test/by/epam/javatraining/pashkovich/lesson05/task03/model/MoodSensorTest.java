09ea7e63621a85fd31793fa1bb0887a3d787d604
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-13 01:12:53 +0300

    Update MoodSensorTest.java

--- lesson05/task3/test/by/epam/javatraining/pashkovich/lesson05/task03/model/MoodSensorTest.java
+++ lesson05/task3/test/by/epam/javatraining/pashkovich/lesson05/task03/model/MoodSensorTest.java
@@ -6,37 +6,8 @@ import static org.junit.Assert.*;
 
 public class MoodSensorTest {
     @Test
-    public void testMoodExcellent() {
-        double moodNumber = 5;
-        String expected = "XD";
-        assertEquals(expected, MoodSensor.mood(moodNumber));
-    }
-
-    @Test
-    public void testMoodGood() {
-        double moodNumber = 0.7;
-        String expected = ":)";
-        assertEquals(expected, MoodSensor.mood(moodNumber));
-    }
-
-    @Test
-    public void testMoodSoSo() {
-        double moodNumber = 0.5;
-        String expected = ":|";
-        assertEquals(expected, MoodSensor.mood(moodNumber));
-    }
-
-    @Test
-    public void testMoodBad() {
-        double moodNumber = 0.3;
-        String expected = ":(";
-        assertEquals(expected, MoodSensor.mood(moodNumber));
-    }
-
-    @Test
-    public void testMoodTerrible() {
-        double moodNumber = -2;
-        String expected = "T_T";
-        assertEquals(expected, MoodSensor.mood(moodNumber));
+    public void testCheckMood() {
+        String expected = "T_T:(:|:)XD";
+        assertTrue(expected.contains(MoodSensor.checkMood()));
     }
 }
