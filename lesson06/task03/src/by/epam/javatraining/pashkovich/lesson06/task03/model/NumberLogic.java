511c55ee35057161557dab0ec831400b43e20ef9
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-13 01:48:32 +0300

    Update NumberLogic.java

--- lesson06/task03/src/by/epam/javatraining/pashkovich/lesson06/task03/model/NumberLogic.java
+++ lesson06/task03/src/by/epam/javatraining/pashkovich/lesson06/task03/model/NumberLogic.java
@@ -2,14 +2,13 @@ package by.epam.javatraining.pashkovich.lesson06.task03.model;
 
 public class NumberLogic {
     public static int calcFactorial(int number) {
-        int result = 1;
+        int result = -1;
 
         if (number >= 0) {
+            result = 1;
             for (int i = 2; i <= number; ++i) {
                 result *= i;
             }
-        } else {
-            result = -1;
         }
 
         return result;
