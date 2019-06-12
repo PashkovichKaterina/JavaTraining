e2498fa6a9b06388ea27c772af5d6c1d3811bab8
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-13 01:14:36 +0300

    Update Main.java

--- lesson05/task3/src/by/epam/javatraining/pashkovich/lesson05/task03/controller/Main.java
+++ lesson05/task3/src/by/epam/javatraining/pashkovich/lesson05/task03/controller/Main.java
@@ -5,6 +5,6 @@ import by.epam.javatraining.pashkovich.lesson05.task03.view.Printer;
 
 public class Main {
     public static void main(String[] args) {
-        Printer.print(MoodSensor.moodRandom());
+        Printer.print(MoodSensor.checkMood());
     }
 }
