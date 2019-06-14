89efc10c70fe18c9307bc3bd01d5acc8ea965e0d
PashkovichKaterina <50631800+PashkovichKaterina@users.noreply.github.com>
2019-06-14 11:28:59 +0300

    Update Main.java

--- maintask01/task01/src/by/epam/javatraining/pashkovich/maintask01/controller/Main.java
+++ maintask01/task01/src/by/epam/javatraining/pashkovich/maintask01/controller/Main.java
@@ -7,8 +7,8 @@ import by.epam.javatraining.pashkovich.maintask01.view.Printer;
 public class Main {
     public static void main(String[] args) {
         MyVector vector = new MyVector(5);
-        vector.addLast(81, 2);
-        vector.addFirst(5, 8, 7, 9, 6, 2, 5, 5, 9);
+        vector.add(81, 2);
+        vector.add(5, 8, 7, 9, 6, 2, 5, 5, 9);
 
         Printer.print("Vector before sort: " + vector.toString());
         vector.sortBubbleDecrease();
